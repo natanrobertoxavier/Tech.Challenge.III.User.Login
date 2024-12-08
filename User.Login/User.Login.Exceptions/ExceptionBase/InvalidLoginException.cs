5a2f148032aed41bbe527d42d415216115635d00
Natan Roberto Xavier <121309547+natanrobertoxavier@users.noreply.github.com>
2024-12-08 16:51:16 -0300

    feat: added initial project

---
+++ User.Login/User.Login.Exceptions/ExceptionBase/InvalidLoginException.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+using User.Persistence.Exceptions;
+
+namespace User.Login.Exceptions.ExceptionBase;
+
+[Serializable]
+public class InvalidLoginException : TechChallengeException
+{
+    public InvalidLoginException() : base(ErrorsMessages.InvalidLogin)
+    {
+    }
+
+    protected InvalidLoginException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
